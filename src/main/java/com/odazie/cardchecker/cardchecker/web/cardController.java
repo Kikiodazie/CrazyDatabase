29a2393db980f7bbe7833c82fa2affaa4bbba4fc
Kikiodazie <kiki.odazie@yahoo.com>
2020-07-18 20:08:58 +0100

    Refactor

--- src/main/java/com/odazie/cardchecker/cardchecker/web/cardController.java
+++ src/main/java/com/odazie/cardchecker/cardchecker/web/cardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class cardController {
 
     private final CardService cardService;
